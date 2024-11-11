af8352c3a9f4e5572beb37286f671e085dd5f8d8
wilsongm <wilsongb02@gmail.com>
2024-11-11 08:54:03 -0400

    Mejorando el diseño de Detalles

--- lib/PokemonDetailScreen.dart
+++ lib/PokemonDetailScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'pokemonTypeColors.dart';
 
 String getPokemonDetail(int id) => """
 query {
@@ -35,6 +36,38 @@ query {
 }
 """;
 
+class PokemonInfoSection extends StatelessWidget {
+  final String title;
+  final List<Widget> content;
+
+  PokemonInfoSection({required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 8.0),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+            Divider(),
+            ...content,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 class PokemonDetailScreen extends StatelessWidget {
   final int id;
@@ -44,11 +77,27 @@ class PokemonDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Detalles de Pokémon', style: TextStyle(color: Colors.white)),
-        backgroundColor: Theme.of(context).primaryColor,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: Query(
+          options: QueryOptions(document: gql(getPokemonDetail(id))),
+          builder: (QueryResult result, {VoidCallback? refetch, FetchMore? fetchMore}) {
+            if (result.isLoading) return AppBar(title: Text('Cargando...'));
+            if (result.hasException) return AppBar(title: Text('Error'));
+
+            final pokemon = result.data?['pokemon_v2_pokemon_by_pk'];
+            final types = pokemon['pokemon_v2_pokemontypes']
+                .map((type) => type['pokemon_v2_type']['name'])
+                .toList();
+            final primaryColor = pokemonTypeColors[types[0]] ?? Colors.blue;
+
+            return AppBar(
+              title: Text('Detalles de Pokémon', style: TextStyle(color: Colors.white)),
+              backgroundColor: primaryColor,
+            );
+          },
+        ),
       ),
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Query(
         options: QueryOptions(document: gql(getPokemonDetail(id))),
         builder: (QueryResult result, {VoidCallback? refetch, FetchMore? fetchMore}) {
@@ -64,63 +113,131 @@ class PokemonDetailScreen extends StatelessWidget {
               .join(', ');
           final abilities = pokemon['pokemon_v2_pokemonabilities']
               .map((ability) => ability['pokemon_v2_ability']['name'])
-              .join(', ');
+              .toList();
           final stats = pokemon['pokemon_v2_pokemonstats'];
           final moves = pokemon['pokemon_v2_pokemonmoves']
               .map((move) => move['pokemon_v2_move']['name'])
-              .join(', ');
+              .toList();
           final generationId = pokemon['pokemon_v2_pokemonspecy']['generation_id'];
-
+          final primaryColor = pokemonTypeColors[types.split(',')[0]] ?? Colors.blue;
 
           return SingleChildScrollView(
             padding: EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Center(
-                  child: Column(
+                Container(
+                  color: primaryColor,
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Image.network(
-                        imageUrl,
-                        width: 150,
-                        height: 150,
-                        fit: BoxFit.contain,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            name[0].toUpperCase() + name.substring(1),
+                            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
+                          ),
+                          Text(
+                            'Tipo: $types',
+                            style: TextStyle(fontSize: 16, color: Colors.white70),
+                          ),
+                          Text(
+                            'Generación: $generationId',
+                            style: TextStyle(fontSize: 16, color: Colors.white70),
+                          ),
+                        ],
                       ),
-                      SizedBox(height: 8),
-                      Text(
-                        name[0].toUpperCase() + name.substring(1),
-                        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.black),
-                      ),
-                      Text(
-                        'Tipo: $types',
-                        style: TextStyle(fontSize: 16, color: Theme.of(context).colorScheme.secondary),
-                      ),
-                      Text(
-                        'Generación: $generationId',
-                        style: TextStyle(fontSize: 16, color: Theme.of(context).colorScheme.secondary),
-                      ),
+                      SizedBox(width: 20),
+                      Image.network(imageUrl, width: 200, height: 200, fit: BoxFit.contain),
                     ],
                   ),
                 ),
-                Divider(),
-                _buildSectionTitle(context, "Información Básica"),
-                SizedBox(height: 8),
-                Text("Altura: ${pokemon['height']} decímetros"),
-                Text("Peso: ${pokemon['weight']} hectogramos"),
-                Divider(),
-                _buildSectionTitle(context, "Habilidades"),
-                Text(abilities),
-                Divider(),
-                _buildSectionTitle(context, "Estadísticas"),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: stats.map<Widget>((stat) {
-                    return Text("${stat['pokemon_v2_stat']['name']}: ${stat['base_stat']}");
-                  }).toList(),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 250,
+                            child: Column(
+                              children: [
+                                PokemonInfoSection(
+                                  title: "Información Básica",
+                                  content: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Icon(Icons.height, color: Colors.grey),
+                                        Text("Altura: ${pokemon['height']} decímetros"),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Icon(Icons.line_weight, color: Colors.grey),
+                                        Text("Peso: ${pokemon['weight']} hectogramos"),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 350,
+                            child: PokemonInfoSection(
+                              title: "Estadísticas",
+                              content: stats.map<Widget>((stat) {
+                                return Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(stat['pokemon_v2_stat']['name']),
+                                    Text("${stat['base_stat']}"),
+                                  ],
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 250,
+                            child: PokemonInfoSection(
+                              title: "Habilidades",
+                              content: abilities.map<Widget>((ability) {
+                                return ListTile(
+                                  leading: Icon(Icons.flash_on, color: Colors.amber),
+                                  title: Text(ability),
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                          Container(
+                            height: 350,
+                            child: PokemonInfoSection(
+                              title: "Movimientos",
+                              content: moves.map<Widget>((move) {
+                                return ListTile(
+                                  leading: Icon(Icons.swap_horiz, color: Colors.green),
+                                  title: Text(move),
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Divider(),
-                _buildSectionTitle(context, "Movimientos"),
-                Text(moves),
               ],
             ),
           );
@@ -128,15 +245,5 @@ class PokemonDetailScreen extends StatelessWidget {
       ),
     );
   }
-
-  Widget _buildSectionTitle(BuildContext context, String title) {
-    return Text(
-      title,
-      style: TextStyle(
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-        color: Theme.of(context).primaryColor,
-      ),
-    );
-  }
 }
+
---
+++ lib/pokemonTypeColors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+Map<String, Color> pokemonTypeColors = {
+  'normal': Colors.grey,
+  'fire': Colors.red,
+  'water': Colors.blue,
+  'electric': Colors.yellow,
+  'grass': Colors.green,
+  'ice': Colors.cyan,
+  'fighting': Colors.brown,
+  'poison': Colors.purple,
+  'ground': Colors.brown,
+  'flying': Colors.blueAccent,
+  'psychic': Colors.pink,
+  'bug': Colors.lightGreen,
+  'rock': Colors.grey[800]!,
+  'ghost': Colors.purple[900]!,
+  'dragon': Colors.deepPurple,
+  'dark': Colors.black,
+  'steel': Colors.grey[400]!,
+  'fairy': Colors.pinkAccent,
+};
