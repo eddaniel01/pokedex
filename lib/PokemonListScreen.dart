d787207252673eae2cf6e4e0f4d46b7ad15b0b7f
eddaniel01 <edrb0001@ce.pucmm.edu.do>
2024-11-23 19:42:10 -0400

    vista del litado principal mejorada

--- lib/PokemonListScreen.dart
+++ lib/PokemonListScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'pokemonTypeColors.dart';
 import 'PokemonDetailScreen.dart';
 
 const String getPokemonList = """
@@ -12,9 +13,17 @@ query {
         name
       }
     }
-     pokemon_v2_pokemonspecy {
+    pokemon_v2_pokemonspecy {
       generation_id
     }
+  }
+}
+""";
+
+const String getPokemonTypes = """
+query {
+  pokemon_v2_type {
+    name
   }
 }
 """;
@@ -25,10 +34,8 @@ class PokemonListScreen extends StatefulWidget {
 }
 
 class _PokemonListScreenState extends State<PokemonListScreen> {
-  final TextEditingController _searchController = TextEditingController();
-  String _filterType = '';
+  String? _selectedType;
   int? _filterGeneration;
-
 
   @override
   Widget build(BuildContext context) {
@@ -36,34 +43,56 @@ class _PokemonListScreenState extends State<PokemonListScreen> {
       appBar: AppBar(
         title: Text('Pokédex', style: TextStyle(color: Colors.white)),
         backgroundColor: Theme.of(context).primaryColor,
-        elevation: 0,
       ),
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                hintText: 'Buscar Pokemon',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+          // Dropdown para seleccionar tipos
+          Query(
+            options: QueryOptions(document: gql(getPokemonTypes)),
+            builder: (QueryResult result, {VoidCallback? refetch, FetchMore? fetchMore}) {
+              if (result.isLoading) return Center(child: CircularProgressIndicator());
+              if (result.hasException) return Center(child: Text(result.exception.toString()));
+
+              final List types = result.data?['pokemon_v2_type'];
+
+              // Agregar "Todos los Tipos" al inicio de la lista
+              final allTypes = [
+                {'name': 'Todos los Tipos'},
+                ...types,
+              ];
+
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButton<String>(
+                  hint: Text("Selecciona un tipo"),
+                  value: _selectedType,
+                  items: allTypes.map<DropdownMenuItem<String>>((type) {
+                    final typeName = type['name'];
+                    return DropdownMenuItem<String>(
+                      value: typeName == 'Todos los Tipos' ? null : typeName,
+                      child: Text(
+                        typeName[0].toUpperCase() + typeName.substring(1),
+                        style: TextStyle(
+                          color: typeName == 'Todos los Tipos'
+                              ? Colors.black
+                              : pokemonTypeColors[typeName] ?? Colors.grey,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedType = value; // Actualizar el tipo seleccionado
+                    });
+                  },
+                  isExpanded: true,
+                  underline: Container(
+                    height: 2,
+                    color: Theme.of(context).primaryColor,
+                  ),
                 ),
-              ),
-              onChanged: (value) {
-                setState(() {
-                  if(int.tryParse(value) != null){
-                    _filterGeneration = int.parse(value);
-                    _filterType = '';
-                  } else {
-                    _filterType = value.toLowerCase();
-                    _filterGeneration = null;
-                  }
-                });
-              },
-            ),
+              );
+            },
           ),
           Expanded(
             child: Query(
@@ -75,19 +104,27 @@ class _PokemonListScreenState extends State<PokemonListScreen> {
 
                 final List pokemons = result.data?['pokemon_v2_pokemon'];
 
-                // Filtrar la lista de Pokémon por tipo o generacion
+                // Filtrar la lista de Pokémon
                 final filteredPokemons = pokemons.where((pokemon) {
                   final types = pokemon['pokemon_v2_pokemontypes']
-                      .map((type) => type['pokemon_v2_type']['name'].toLowerCase())
+                      .map((type) => type['pokemon_v2_type']['name'])
                       .toList();
                   final generationId = pokemon['pokemon_v2_pokemonspecy']?['generation_id'];
 
-                  bool matchesType = types.contains(_filterType) || _filterType.isEmpty;
+                  bool matchesType = _selectedType == null || types.contains(_selectedType);
                   bool matchesGeneration = _filterGeneration == null || generationId == _filterGeneration;
                   return matchesType && matchesGeneration;
                 }).toList();
 
-                return ListView.builder(
+                // Mostrar Pokémon en una cuadrícula
+                return GridView.builder(
+                  padding: const EdgeInsets.all(8.0),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 3 / 4,
+                  ),
                   itemCount: filteredPokemons.length,
                   itemBuilder: (context, index) {
                     final pokemon = filteredPokemons[index];
@@ -95,54 +132,42 @@ class _PokemonListScreenState extends State<PokemonListScreen> {
                     final imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon['id']}.png';
                     final types = pokemon['pokemon_v2_pokemontypes']
                         .map((type) => type['pokemon_v2_type']['name'])
-                        .join(', ');
-                    final generationId = pokemon['pokemon_v2_pokemonspecy']['generation_id'];
-                    return Card(
-                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                      elevation: 5,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                      color: Theme.of(context).cardColor,
-                      child: ListTile(
-                        contentPadding: EdgeInsets.all(16),
-                        leading: Hero(
-                          tag: 'pokemon-image-${pokemon['id']}',
-                          child: Image.network(
-                            imageUrl,
-                            width: 60,
-                            height: 60,
-                            fit: BoxFit.contain,
-                          )
-                        ),
-                        title: Text(
-                          name[0].toUpperCase() + name.substring(1),
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                        subtitle: Text(
-                          '$types | Generación: $generationId',
-                          style: Theme.of(context).textTheme.bodyLarge,
-                        ),
-                        trailing: Icon(Icons.arrow_forward, color: Theme.of(context).colorScheme.secondary),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            PageRouteBuilder(
-                              pageBuilder: (context, animation, secondaryAnimation) => PokemonDetailScreen(id: pokemon['id']),
-                              transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                                const begin = Offset(-1.0, 0.0);
-                                const end = Offset.zero;
-                                const curve = Curves.ease;
+                        .toList();
+                    final primaryType = types[0];
+                    final primaryColor = pokemonTypeColors[primaryType] ?? Colors.grey;
 
-                                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-                                var offsetAnimation = animation.drive(tween);
-
-                                return SlideTransition(
-                                  position: offsetAnimation,
-                                  child: child,
-                                );
-                              },
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PokemonDetailScreen(id: pokemon['id']),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                        color: primaryColor,
+                        elevation: 5,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.white.withOpacity(0.5),
+                              radius: 50,
+                              backgroundImage: NetworkImage(imageUrl),
                             ),
-                          );
-                        },
+                            SizedBox(height: 10),
+                            Text(
+                              name[0].toUpperCase() + name.substring(1),
+                              style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              primaryType.toUpperCase(),
+                              style: TextStyle(color: Colors.white70, fontSize: 14),
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
